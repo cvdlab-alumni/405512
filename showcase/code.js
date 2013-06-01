4801bf844332d51e6ca0add09a41ab45ea7e1526
Eder Monaco <reager.tug@gmail.com>
2013-06-01 19:20:26 +0200

    finished upper body

--- showcase/code.js
+++ showcase/code.js
@@ -1,5 +1,6 @@
 /*
-	LEGO 
+	LEGO
+	@Author: Eder Monaco
 */
 
 //	AUXILIARY VARIABLES AND FUNCTIONS
@@ -14,7 +15,7 @@ var cylinder = function(radius, height, quality) {
 	var disk = DISK(radius)([quality, 2]);
 	var result = EXTRUDE([height])(disk);;
 	return result;
-}
+};
 
 // BASE
 
@@ -23,9 +24,10 @@ function createBase(width, depth, color) {
 	var cylinderTop = cylinder(0.25, 0.25, 20);
 	var baseUnit = STRUCT([cuboid, T([0,1,2])([0.5,1,0.5])(R([1,2])(-PI/2)(cylinderTop))]);
 	var baseRow = STRUCT(REPLICA(width)([baseUnit, T([0])([1])]));
+	//result
 	var base = COLOR(color)(STRUCT(REPLICA(depth)([baseRow, T([2])([1])])));
 	return base;
-}
+};
 
 // LOWER BODY CHARACTER
 
@@ -46,7 +48,7 @@ function createLowerBodyCharacter(color){
 	//result
 	var lowerBody = COLOR(color)(STRUCT([legs, belt]));
 	return lowerBody;
-}
+};
 
 // UPPER BODY CHARACTER
 
@@ -68,19 +70,76 @@ function createUpperBodyCharacter(color){
 	var bodyTopBezier = MAP(BEZIER(S1)([c4,c5]))(domain);
 	var bodyTop = T([0,1,2])([0,4.7,0])(bodyTopBezier);
 	//arms
+	var domain2D = DOMAIN([[0,1],[0,1]])([40,40]);
+	var profile_armRight1 = BEZIER(S0)([[0.3, 4.45, 0.7], [-0.3, 4.45, 0.7], [-0.3, 3.5, 0.7], [-0.2, 3.5, 1.2]]);
+	var profile_armRight2 = BEZIER(S0)([[0.3, 4.45, 0.3], [-0.3, 4.45, 0.3], [-0.3, 3, 0.2], [-0.2, 3.2, 1.2]]);
+	var profile_armRight12UP = BEZIER(S0)([[0.3, 4.7, 0.45], [-0.5, 4.7, 0.45], [-0.5, 3.35, 0.45], [-0.5, 3.35, 1.2]]);
+	var profile_armRight12DOWN = BEZIER(S0)([[0.3, 3.8, 0.45], [0.3, 3.8, 0.45], [0.3, 3.4, 0.45], [0.3, 3.4, 1.2]]);
+	var armRightSurface1 = MAP(BEZIER(S1)([profile_armRight1, profile_armRight12UP, profile_armRight2]))(domain2D);
+	var armRightSurface2 = MAP(BEZIER(S1)([profile_armRight1, profile_armRight12DOWN, profile_armRight2]))(domain2D);
+	var armRight = STRUCT([armRightSurface1, armRightSurface2]);
+	var profile_armLeft1 = BEZIER(S0)([[-0.3, 4.45, 0.7], [0.3, 4.45, 0.7], [0.3, 3.5, 0.7], [0.2, 3.5, 1.2]]);
+	var profile_armLeft2 = BEZIER(S0)([[-0.3, 4.45, 0.3], [0.3, 4.45, 0.3], [0.3, 3, 0.2], [0.2, 3.2, 1.2]]);
+	var profile_armLeft12UP = BEZIER(S0)([[-0.3, 4.7, 0.45], [0.5, 4.7, 0.45], [0.5, 3.35, 0.45], [0.5, 3.35, 1.2]]);
+	var profile_armLeft12DOWN = BEZIER(S0)([[-0.3, 3.8, 0.45], [-0.3, 3.8, 0.45], [-0.3, 3.4, 0.45], [-0.3, 3.4, 1.2]]);
+	var armLeftSurface1 = MAP(BEZIER(S1)([profile_armLeft1, profile_armLeft12UP, profile_armLeft2]))(domain2D);
+	var armLeftSurface2 = MAP(BEZIER(S1)([profile_armLeft1, profile_armLeft12DOWN, profile_armLeft2]))(domain2D);
+	var armLeft = T([0,1,2])([2,0,0])(STRUCT([armLeftSurface1, armLeftSurface2]));
+	
+	//result
+	var upperBody = COLOR(color)(STRUCT([bodyBack, bodyFront, bodyRight, bodyLeft, bodyTop, armRight, armLeft]));
+	return upperBody;
+};
 
-	//hands
+// HANDS
+
+function createHands(color){
+	//hand
+	var domain = PROD1x1([INTERVALS(1)(20),INTERVALS(1)(20)]);
+	var c0 = BEZIER(S0)([[0,0,0],[2,0,0]]);
+	var c1 = BEZIER(S0)([[0.3,1.6,0.2],[1.7,1.6,0.2]]);
+	var bodyBackBezier = MAP(BEZIER(S1)([c0,c1]))(domain);
+	var bodyBack = T([0,1,2])([0,3.1,0])(bodyBackBezier);
+	var bodyFront = T([0,1,2])([2,0,1])(R([0,2])(PI)(bodyBack));
+	var c2 = BEZIER(S0)([[0,0,0],[0,0,1]]);
+	var c3 = BEZIER(S0)([[0.3,1.6,0.2],[0.3,1.6,0.8]]);
+	var bodyRightBezier = MAP(BEZIER(S1)([c2,c3]))(domain);
+	var bodyRight = T([0,1,2])([0,3.1,0])(bodyRightBezier);
+	var bodyLeft = T([0,1,2])([2,0,1])(R([0,2])(PI)(bodyRight));
+	var c4 = BEZIER(S0)([[0.3,0,0.2],[1.7,0,0.2]]);
+	var c5 = BEZIER(S0)([[0.3,0,0.8],[1.7,0,0.8]]);
+	var bodyTopBezier = MAP(BEZIER(S1)([c4,c5]))(domain);
+	var bodyTop = T([0,1,2])([0,4.7,0])(bodyTopBezier);
 
 	//result
-	var upperBody = COLOR(color)(STRUCT([bodyBack, bodyFront, bodyRight, bodyLeft, bodyTop]));
-	return upperBody;
-}
+	var hands = COLOR(color)(STRUCT([bodyBack, bodyFront, bodyRight, bodyLeft, bodyTop]));
+	return hands;
+};
+
+// HEAD
+
+function createHead(color){
+	//neck
+	var neck = T([0,1,2])([1,4.7,0.5])(R([1,2])(-PI/2)(cylinder(0.3, 1.49, 20)));
+	//head
+	var domainFace = DOMAIN([[0,1],[0,2*PI]])([30,50]);
+	var c0 = BEZIER(S0)([[0, 0, 0.6],[0.05, 0, 0.6], [0.65, 0, 0.6], [0.65, 0, 0.6], [0.65, 0, 0]]);
+	var mapping = ROTATIONAL_SURFACE(c0);
+	var faceSurfaceUP = MAP(mapping)(domainFace);
+	var faceSurfaceDOWN = R([1,2])(PI)(faceSurfaceUP);
+	var faceSurface = T([0,1,2])([1,5.4,0.5])(R([1,2])(-PI/2)(STRUCT([faceSurfaceDOWN, faceSurfaceUP])));
+	//result
+	var head = COLOR(color)(STRUCT([neck, faceSurface]));
+	return head;
+};
 
 // CHARACTER
 
 var lowerBody = createLowerBodyCharacter(blueColor);
 var upperBody = createUpperBodyCharacter(redColor);
-var character = STRUCT([lowerBody, upperBody]);
+var hands = createHands(yellowColor);
+var head = createHead(yellowColor);
+var character = STRUCT([lowerBody, upperBody, head]);
 
 //	FINAL MODEL
 
